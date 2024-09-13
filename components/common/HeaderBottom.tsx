a34ca7a7db20f494e582b79a2667742104c06025
chaloemchai-beer <Kasutochose1@gmail.com>
2024-09-13 19:56:12 +0700

    test deploy

--- components/common/HeaderBottom.tsx
+++ components/common/HeaderBottom.tsx
@@ -1,23 +1,31 @@
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 
-const HeaderBottom = ({ menus }: any) => {
+// Define the shape of a menu item
+interface MenuItem {
+  name: string;
+}
+
+interface HeaderBottomProps {
+  menus: MenuItem[];
+}
+
+const HeaderBottom = ({ menus }: HeaderBottomProps) => {
   return (
     <div
-      className="w-full h-10 bg-gray-900 text-sm text-white px-4 flex
-    items-center"
+      className="w-full h-10 bg-gray-900 text-sm text-white px-4 flex items-center"
     >
       <p
         className="flex items-center gap-1 h-8 px-2 border border-transparent
-        hover:border-white cursor-pointer duration-300 "
+        hover:border-white cursor-pointer duration-300"
       >
         <MenuOutlinedIcon />
         All
       </p>
-      {menus.map((menu: any) => (
+      {menus.map((menu) => (
         <p
           key={menu.name}
-          className="hidden md:inline-flex flex items-center gap-1 h-8 px-2 border border-transparent
- hover:border-white cursor-pointer duration-300"
+          className="hidden md:inline-flex items-center gap-1 h-8 px-2 border border-transparent
+          hover:border-white cursor-pointer duration-300"
         >
           {menu.name}
         </p>
